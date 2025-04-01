a37126195f63582ca7b645fa8074f84f2805199e
ptrksn <111421878+ptrksn@users.noreply.github.com>
2025-04-01 16:46:35 +0200

    Update start.py

--- start.py
+++ start.py
@@ -9,14 +9,10 @@ if len(sys.argv) > 1:
 
     if task == "rsi":
         print("▶️ Starte RSI-Skript...")
-        result = subprocess.run(["python", "phemex_rsi_auth.py"], capture_output=True, text=True)
-        print(result.stdout)
-        print(result.stderr)
+        subprocess.run(["python", "phemex_rsi_auth.py"])
     elif task == "watchdog":
         print("▶️ Starte Watchdog-Skript...")
-        result = subprocess.run(["python", "phemex_watchdog.py"], capture_output=True, text=True)
-        print(result.stdout)
-        print(result.stderr)
+        subprocess.run(["python", "phemex_watchdog.py"])
     else:
         print(f"❌ Unbekannte Aufgabe: {task}")
 else:
