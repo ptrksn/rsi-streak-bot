d4572527df6432307844daf8c70c48e1c82f590a
ptrksn <111421878+ptrksn@users.noreply.github.com>
2025-04-01 15:51:17 +0200

    Update start.py

--- start.py
+++ start.py
@@ -4,10 +4,13 @@ import subprocess
 
 if len(sys.argv) > 1:
     task = sys.argv[1]
+    print(f"📦 Ausgewählter Task: {task}")  # <-- NEU
 
     if task == "rsi":
+        print("▶️ Starte RSI-Skript...")
         subprocess.run(["python", "phemex_rsi_auth.py"])
     elif task == "watchdog":
+        print("▶️ Starte Watchdog-Skript...")
         subprocess.run(["python", "phemex_watchdog.py"])
     else:
         print(f"❌ Unbekannte Aufgabe: {task}")
