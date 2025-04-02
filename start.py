45b91432d100a30e32ef02bb621ab78e31806c5b
ptrksn <111421878+ptrksn@users.noreply.github.com>
2025-04-02 17:04:27 +0200

    Update start.py

--- start.py
+++ start.py
@@ -12,12 +12,7 @@ if len(sys.argv) > 1:
         result = subprocess.run(["python3", "tradingview_rsi_streak.py"], capture_output=True, text=True)
         print(result.stdout)
         print(result.stderr)
-    elif task == "watchdog":
-        print("▶️ Starte Watchdog-Skript...")
-        result = subprocess.run(["python3", "phemex_watchdog.py"], capture_output=True, text=True)
-        print(result.stdout)
-        print(result.stderr)
     else:
         print(f"❌ Unbekannte Aufgabe: {task}")
 else:
-    print("❌ Kein Argument übergeben. Bitte z. B. 'rsi' oder 'watchdog' angeben.")
+    print("❌ Kein Argument übergeben. Bitte z. B. 'rsi' angeben.")
